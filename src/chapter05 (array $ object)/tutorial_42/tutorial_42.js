0f8cbbdb6a0ac7763037ffacd373e1efb3be13a6
Hoang Phuc <hoangphuc311tn@gmail.com>
2026-02-20 10:04:57 +0700

    video43

--- src/chapter05 (array $ object)/tutorial_42/tutorial_42.js
+++ src/chapter05 (array $ object)/tutorial_42/tutorial_42.js
@@ -4,3 +4,21 @@
 console.log("Tutorial 42")
 
 
+const person = {
+    name: "hoangphuc",
+    age: 25,
+}
+
+
+
+console.log('person before:', person)
+console.log('person:', person.name)
+console.log('person:', person.age)
+
+
+person.address = "tayninh"
+console.log('person after:', person)
+
+
+delete person.name;
+console.log('person after:', person)
