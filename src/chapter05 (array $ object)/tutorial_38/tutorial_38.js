6b7bd59c17bc9265e8612aa23c7318fea0267df9
Hoang Phuc <hoangphuc311tn@gmail.com>
2026-02-20 09:35:29 +0700

    video40

--- src/chapter05 (array $ object)/tutorial_38/tutorial_38.js
+++ src/chapter05 (array $ object)/tutorial_38/tutorial_38.js
@@ -1,6 +1,18 @@
 // ----------------------
 
 
-console.log("Tutorial 37")
+console.log("Tutorial 38")
 
 
+const scores = [10, 9, 8, 7, 6, 5, 4];
+
+// read data
+// scores.forEach((value, index)=>{
+//     console.log(`value = ${value}, index = ${index}`)
+// })
+
+// modify data
+const scores2 = scores.map((value, index) => {
+    return value * 2 
+})                                   
+console.log('scores:', scores2)
