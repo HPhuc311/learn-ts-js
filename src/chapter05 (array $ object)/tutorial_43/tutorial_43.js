ee25b2227ae36c73fadc108276bcabe7f3e5d410
Hoang Phuc <hoangphuc311tn@gmail.com>
2026-02-20 15:59:43 +0700

    video44

--- src/chapter05 (array $ object)/tutorial_43/tutorial_43.js
+++ src/chapter05 (array $ object)/tutorial_43/tutorial_43.js
@@ -1,6 +1,50 @@
-                            // ----------------------
+// ----------------------
 
 
-console.log("Tutorial 37")
+console.log("Tutorial 43")
+
+const sv1 = {
+    username: "hoangphuc",
+    age: 23,
+}
+const sv2= {
+    username: "hoangphuc1",
+    age: 234,
+}
+const sv3 = {
+    username: "hoangphuc2",
+    age: 253,
+}
 
 
+const sinhvien = [sv1, sv2, sv3]
+console.log('sinhvien:', sinhvien)
+
+
+// sinhvien.forEach((item,index) => {
+//     console.log(`index = ${index}, name = ${item.username}`)
+    
+// })
+
+// for...in: duyệt qua thuộc tính 
+// for...of: duyệt qua giá trị
+
+const person = {
+    email: "hoangphhuc311tn@gmail.com",
+    address: "tayninh",                     
+    phone: 5,
+}
+
+
+// for (let key in person){
+//     console.log('key:', key, person[key])
+
+// }
+
+for (let value of Object.values(person)) {
+    console.log(value);
+}
+
+// for (let [key, value] of Object.entries(person)) {
+//     console.log(key, value);
+// }
