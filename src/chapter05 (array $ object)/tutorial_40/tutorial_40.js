1cdda05d3db2a0a2c3db31d3a3c7622cbb3a9e26
Hoang Phuc <hoangphuc311tn@gmail.com>
2026-02-20 09:43:19 +0700

    video41

--- src/chapter05 (array $ object)/tutorial_40/tutorial_40.js
+++ src/chapter05 (array $ object)/tutorial_40/tutorial_40.js
@@ -1,6 +1,22 @@
 // ----------------------
 
 
-console.log("Tutorial 37")
+console.log("Tutorial 40")
 
 
+const ages = [10, 20, 30, 40, 12, 11, 19, 50, 60];
+
+const agesX2 = ages.map((item, index) => {
+    return item * 2
+})
+
+const agesX18 = ages.filter((item, index) => {
+    return item > 18 // true 
+})
+
+
+console.log(`original: ${ages}`)
+console.log(`agesX2: ${agesX2}`)
+
+
+console.log('agesX18:', agesX18)
