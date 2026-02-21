9082d99a4100776e00662e2e3d71572161715866
Hoang Phuc <hoangphuc311tn@gmail.com>
2026-02-21 09:21:34 +0700

    lab03

--- src/chapter05 (array $ object)/tutorial_44/tutorial_44.js
+++ src/chapter05 (array $ object)/tutorial_44/tutorial_44.js
@@ -1,6 +1,117 @@
 // ----------------------
 
 
-console.log("Tutorial 37")
+console.log("Tutorial 44 Lab")
 
+const products = [
+    {
+        name: "T-shirt",
+        price: 200,
+        inStock: true
+    },
+    {
+        name: "Shirt",
+        price: 250,
+        inStock: false
+    },
+    {
+        name: "Trouser",
+        price: 200,
+        inStock: true
+    },
+    {
+        name: "Skirt",
+        price: 300,
+        inStock: false
+    },
+    {
+        name: "Shoes",
+        price: 200,
+        inStock: true
+    },
+    {
+        name: "Short",
+        price: 200,
+        inStock: false
+    },
+    {
+        name: "Dress",
+        price: 200,
+        inStock: true
+    }
+]
+// =============================================
+
+console.log("In ra tên sản phẩm đầu tiên")
+
+console.log(products[0].name)
+
+// =============================================
+
+console.log("Thay đổi giá sản phẩm thứ hai thành 150 và in ra danh sách tất cả sản phẩm")
+
+products[1].price = 150;
+console.log('products:', products)
+
+// =============================================
+
+console.log("Thêm một sản phẩm mới vào cuối mảng và in ra danh sách tất cả sản phẩm")
+
+products.push({
+    name: "Blue-Dress",
+    price: 400,
+    inStock: true
+})
+console.log('products:', products)
+
+// =============================================
+
+console.log("Xoá sản phẩm cuối cùng ra khỏi danh sách và in ra danh sách tất cả sản phẩm")
+
+products.pop()
+console.log('products:', products)
+
+// =============================================
+
+console.log("Dùng forEach() để in ra tất cả tên sản phẩm")
+
+products.forEach((item, index) => {
+    console.log('products:', item)
+})
+
+// =============================================
+
+console.log("Dùng map() để tạo mảng mới chỉ chứa giá sản phẩm.")
+
+const priceProduct = products.map((item, index) => {
+    return item.price
+})
+
+console.log(priceProduct)
+
+// =============================================
+
+console.log("Dùng filter() để lấy các sản phẩm còn hàng(inStock = true)")
+
+const inStockProducts = products.filter((item, index) => item.inStock === true)
+console.log('inStockProducts:', inStockProducts)
+
+// =============================================
+
+console.log("Dùng for...in để duyệt qua thuộc tính của sản phẩm đầu tiên")
+
+for (let key in products[0]) {
+    console.log(key, products[0][key]);
+}
+
+
+// const v1 = {
+//     name: "Dress",
+//     price: 200,
+//     inStock: true
+// }
+
+// for (let value of Object.values(v1)) {
+//     console.log(value);
+// }
 
